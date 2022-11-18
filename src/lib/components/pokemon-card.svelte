f8968399df7b063f9b64f662fec8dc819e323fcb
Nick Cheatwood <nick.cheatwood@gmail.com>
2022-11-17 19:47:16 -0600

    feat: remove fade effect

--- src/lib/components/pokemon-card.svelte
+++ src/lib/components/pokemon-card.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
 	import type { Pokemon } from '../@types/Pokemon';
 	export let pokemon: Pokemon;
 </script>
@@ -7,7 +6,6 @@
 <a
 	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
 	href={`/pokemon/${pokemon.id}`}
-	transition:fade
 >
 	<img class="h-40 w-40" src={pokemon.imageUrl} alt={pokemon.name} />
 	<h2 class="uppercase text-xl">{pokemon.id}. {pokemon.name}</h2>
